8268fd37bce83323d0bc5738e954970b5ba6da3f
WhatsMusic.de <info@whatsmusic.de>
2025-01-23 11:52:34 +0100

    Open Graph und favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,20 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Tutitory - Dein Tutorial-Generator",
   description: "Erstelle detaillierte Tutorials basierend auf deinen Themen.",
+  openGraph: {
+    title: "Tutitory - Dein Tutorial-Generator",
+    description: "Erstelle detaillierte Tutorials basierend auf deinen Themen.",
+    url: "https://tutitory.com", // Die URL deiner Webseite
+    type: "website",
+    images: [
+      {
+        url: "/repository-open-graph-tutitory-ki-tutorial-generator.jpg", // Pfad zum Bild
+        width: 1200, // Standard-Breite für Open Graph-Bilder
+        height: 630,  // Standard-Höhe für Open Graph-Bilder
+        alt: "Tutitory: Dein KI-Tutorial-Generator",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -20,15 +34,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
+      <head>
+        <meta property="og:title" content="Tutitory - Dein Tutorial-Generator" />
+        <meta
+          property="og:description"
+          content="Erstelle detaillierte Tutorials basierend auf deinen Themen."
+        />
+        <meta property="og:url" content="https://www.tutitory.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="/repository-open-graph-tutitory-ki-tutorial-generator.jpg"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:image:alt"
+          content="Tutitory: Dein KI-Tutorial-Generator"
+        />
+      </head>
       <body className={`bg-gray-50 text-gray-800 ${inter.className}`}>
         <header className="bg-blue-500 text-white p-4">
           <div className="container mx-auto flex justify-between items-center">
-            <h1 className="text-xl font-bold"> <Link href="/" className="text-white hover:text-white">Tutitory</Link></h1>
+            <h1 className="text-xl font-bold">
+              <Link href="/" className="text-white hover:text-white">
+                Tutitory
+              </Link>
+            </h1>
           </div>
         </header>
-        <main className="container mx-auto p-6">
-          {children}
-        </main>
+        <main className="container mx-auto p-6">{children}</main>
         <footer className="bg-gray-800 text-white p-4 mt-6">
           <div className="container mx-auto text-center">
             © 2025 Tutitory, All rights reserved.
@@ -41,6 +76,5 @@ export default function RootLayout({
         </footer>
       </body>
     </html>
-
   );
 }
